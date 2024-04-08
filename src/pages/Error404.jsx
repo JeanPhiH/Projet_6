89902d3fb26a6882c6c740f7ce75e751279777b1
JeanPhiH <jp.herfeld@gmail.com>
2024-04-08 14:18:49 +0200

    page error 404 ok

--- src/pages/Error404.jsx
+++ src/pages/Error404.jsx
@@ -1,9 +1,11 @@
-
+import { Link } from "react-router-dom";
 
 export default function Error404 () {
 	return (
-		<>
-			<h1>Error404</h1>
-		</>
+		<div className="error404">
+			<h2 className="title404">404</h2>
+			<p className="p404">Oups! La page que vous demandez n’existe pas.</p>
+			<Link to="/" className="link404">Retourner sur la page d’accueil</Link>
+		</div>
 	);
 }
