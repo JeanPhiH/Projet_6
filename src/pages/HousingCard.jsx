a161cd8a865c6c3deb52c396b61ec8e6f2af9ad8
JeanPhiH <jp.herfeld@gmail.com>
2024-04-12 16:30:46 +0200

    error404 fixed

--- src/pages/HousingCard.jsx
+++ src/pages/HousingCard.jsx
@@ -3,29 +3,25 @@ import Carousel from "../components/Carousel";
 import Details from "../components/Details";
 import PropTypes from 'prop-types';
 import Error404 from "./Error404";
-import { useState, useEffect } from 'react';
 
 export default function HousingCard ({listHousings, isLoading}) {
   const {id} = useParams();
-  
-  const [idHousing, setIdHousing] = useState(null);
-  useEffect(() => {
-    const foundHousing = listHousings.find(housing => housing.id === id);
-    setIdHousing(foundHousing);
-  }, [listHousings, id]);
-	console.log(isLoading);
-
+	const idHousing = listHousings.find(housing => housing.id === id);
+	console.log(idHousing);
+	console.log(id);
+	
   return (
-    <>
-      {isLoading ? <p>Loading...</p> : (
-        !idHousing ? <Error404 /> : (
+		<>
+      {idHousing ? (
           <>
             <Carousel key={idHousing.title} idHousing={idHousing}/>
 
 						<Details key={idHousing.location} idHousing={idHousing}/>
           </>
-        )
-      )}
+        ) : (
+					!isLoading && <Error404 />
+				)
+			}
     </>
   );
 }
