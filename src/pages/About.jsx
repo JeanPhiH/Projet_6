9b10c69db2c40bfcc3e5607a6447127adef10553
JeanPhiH <jp.herfeld@gmail.com>
2024-04-15 15:07:28 +0200

    studio fonctionnel fixed + meta tags

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,20 +1,11 @@
 import Collapse from "../components/Collapse";
 import Banner from "../components/Banner";
-// import { useEffect, useState } from "react";
 import about from "../../public/assets/about.json";
 
-
 export default function About () {
-	// const [jsonAbout, setJsonAbout] = useState([]);
 	const bannerTitle = "";
 	const bgbanner = {backgroundColor: "rgba(0, 0, 0, 0.3)"};
 	const bgurl= "/assets/image_source_2_1240.webp";
-	// useEffect(() => {
-	// 	fetch("/assets/about.json")
-	// 	.then((response) => response.json())
-	// 	.then((jsonData) => {
-	// 	setJsonAbout(jsonData);
-	// })}, []);
 	
 	return (
 		<>
